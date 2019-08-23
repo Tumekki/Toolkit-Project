fd37e5ac85deccd095efc075ec98f610e4d63aea
Tuukka <tospartanen@gmail.com>
2019-08-23 11:59:13 +0300

    Added functionality to converter

--- Converter/src/com/company/Converter.java
+++ Converter/src/com/company/Converter.java
@@ -10,6 +10,16 @@ public class Converter {
         this.convertable = convertable;
     }
 
+    public double toFahrenheit(double convertable){
+        double result = (convertable * 9.0/5.0) + 32;
+        return result;
+    }
+
+    public double toCelsius(double convertable){
+        double result = (convertable - 32) * 5.0/9.0;
+        return result;
+    }
+
 
 
     private double convertable;
--- Converter/src/com/company/GUI.java
+++ Converter/src/com/company/GUI.java
@@ -1,14 +1,18 @@
 package com.company;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import javax.swing.*;
 import javax.swing.border.Border;
+import javax.xml.ws.Action;
 
 
 public class GUI extends JFrame{
 
     public GUI(){
-        this.setSize(800,400);
+        Converter newConverter = new Converter();
 
+        this.setSize(800,400);
         this.setLocationRelativeTo(null);
         Toolkit tk = Toolkit.getDefaultToolkit();
         Dimension dim = tk.getScreenSize();
@@ -27,19 +31,48 @@ public class GUI extends JFrame{
         String[] temperatureChoices = {"Celsius to Fahrenheit", "Fahrenheit to Celsius"};
         JComboBox temperatures = new JComboBox(temperatureChoices);
         temperatures.setSelectedIndex(0);
+
         temperatures.setMaximumSize(new Dimension(150,40));
 
-        JLabel celsius = new JLabel("Celsius");
-        celsius.setAlignmentX(Component.CENTER_ALIGNMENT);
+        JLabel label1 = new JLabel("Celsius");
+        label1.setAlignmentX(Component.CENTER_ALIGNMENT);
         JTextField toConvert = new JTextField();
         toConvert.setMaximumSize(new Dimension(150,20));
 
 
         JButton convert = new JButton("=");
-        JLabel fahrenheit = new JLabel("Fahrenheit");
-        fahrenheit.setAlignmentX(Component.CENTER_ALIGNMENT);
+        JLabel label2 = new JLabel("Fahrenheit");
+        label2.setAlignmentX(Component.CENTER_ALIGNMENT);
         JTextField converted = new JTextField();
         converted.setMaximumSize(new Dimension(150,20));
+
+        //Add a listener to the option box so that the celsius and fahrenheit labels swap places depending on
+        //the converted and convertable
+        temperatures.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if(temperatures.getSelectedItem() == "Celsius to Fahrenheit"){
+                    label1.setText("Celsius");
+                    label2.setText("Fahrenheit");
+                } else if(temperatures.getSelectedItem() == "Fahrenheit to Celsius") {
+                    label1.setText("Fahrenheit");
+                    label2.setText("Celsius");
+                }
+            }
+        });
+
+        //Add functionality to the "=" button.
+        convert.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if(temperatures.getSelectedItem() == "Celsius to Fahrenheit"){
+                    String temp = toConvert.getText();
+                    newConverter.setConvertable(Double.parseDouble(temp));
+                    double temp2 = newConverter.toFahrenheit(newConverter.getConvertable());
+                    converted.setText(Double.toString(temp2));
+                }
+            }
+        });
 
 
         mainLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
@@ -49,14 +82,13 @@ public class GUI extends JFrame{
         mainPanel.add(Box.createRigidArea(new Dimension(0,30)));
         mainPanel.add(temperatures);
         mainPanel.add(Box.createRigidArea(new Dimension(0,30)));
-       // mainPanel.add(toConvert);
-        mainPanel.add(celsius);
+        mainPanel.add(label1);
 
         mainPanel.add(toConvert);
         mainPanel.add(Box.createRigidArea(new Dimension(0,10)));
         mainPanel.add(convert);
         mainPanel.add(Box.createRigidArea(new Dimension(0,10)));
-        mainPanel.add(fahrenheit);
+        mainPanel.add(label2);
         mainPanel.add(converted);
         mainPanel.add(Box.createRigidArea(new Dimension(0,30)));
 

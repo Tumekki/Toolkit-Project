6cae915f5f30d7eb07931b37fd51c2b7937ded54
Tuukka <tospartanen@gmail.com>
2019-08-29 11:36:09 +0300

    basic GUI for weight converter

---
+++ toolkitproject/src/main/java/com/company/WeightConverter.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class WeightConverter {
+
+    private double convertable;
+
+
+    public double getConvertable() {
+        return convertable;
+    }
+    public void setConvertable(double convertable) {
+        this.convertable = convertable;
+    }
+
+
+
+}
--- toolkitproject/src/main/java/com/company/WeightConverterGUI.java
+++ toolkitproject/src/main/java/com/company/WeightConverterGUI.java
@@ -6,6 +6,7 @@ import java.awt.*;
 public class WeightConverterGUI extends JFrame{
     public WeightConverterGUI(){
 
+        WeightConverter converter = new WeightConverter();
         this.setSize(600,250);
         this.setLocationRelativeTo(null);
         Toolkit tk = Toolkit.getDefaultToolkit();
@@ -18,11 +19,39 @@ public class WeightConverterGUI extends JFrame{
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setTitle("Weight Converter");
 
+        JPanel mainPanel = new JPanel();
+
+        JPanel leftPanel = new JPanel();
+        JPanel rightPanel = new JPanel();
+        leftPanel.setLayout(new BoxLayout(leftPanel, BoxLayout.PAGE_AXIS));
+        rightPanel.setLayout(new BoxLayout(rightPanel, BoxLayout.PAGE_AXIS));
+
+        JTextField leftInput = new JTextField("left");
+        leftInput.setHorizontalAlignment(JTextField.RIGHT);
+        String[] weightChoices = {"Kilogram", "Pound", "Stone", "Ounce"};
+        JComboBox leftTypes = new JComboBox(weightChoices);
+        leftTypes.setSelectedIndex(0);
+        leftPanel.add(leftInput);
+        leftPanel.add(leftTypes);
+
+        JTextField rightInput = new JTextField("right");
+        rightInput.setHorizontalAlignment(JTextField.RIGHT);
+        String[] weightChoices2 = {"Kilogram", "Pound", "Stone", "Ounce"};
+        JComboBox rightTypes = new JComboBox(weightChoices2);
+        rightTypes.setSelectedIndex(1);
+        rightPanel.add(rightInput);
+        rightPanel.add(rightTypes);
+
+        JLabel equals = new JLabel("=");
+        equals.setFont(new Font("Arial", Font.PLAIN, 20));
+
+        mainPanel.add(Box.createRigidArea(new Dimension(0,30)));
+        mainPanel.add(leftPanel);
+        mainPanel.add(equals);
+        mainPanel.add(rightPanel);
 
 
-
-
-
+        this.add(mainPanel);
         this.setVisible(true);
     }
 }
